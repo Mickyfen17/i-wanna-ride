4e0398e55267fa7b89db47d5b77be0ad751243ac
Michael Fenwick <mickyfen17@aol.com>
2017-04-10 10:48:06 -0600

    add Header component to use throughout app

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Header from './Header';
+
 
 export default class Login extends Component  {
   constructor() {
@@ -24,28 +26,31 @@ export default class Login extends Component  {
   }
   render() {
     return (
-      <article className='login-card'>
-        <h1>Login</h1>
-        <input
-          className='login-input'
-          type='text'
-          value={ this.state.username }
-          name='username'
-          onChange={ e => this.handleUserInput(e) }
-        />
-        <input
-          className='login-input'
-          type='password'
-          value={ this.state.password }
-          name='password'
-          onChange={ e => this.handleUserInput(e) }
-        />
-        <button
-          className='login-button'
-          onClick={ () => this.handleUserSubmit() } >
-          Login
-        </button>
-      </article>
+      <div>
+        <Header />
+        <article className='login-card'>
+          <h1>Login</h1>
+          <input
+            className='login-input'
+            type='text'
+            value={ this.state.username }
+            name='username'
+            onChange={ e => this.handleUserInput(e) }
+          />
+          <input
+            className='login-input'
+            type='password'
+            value={ this.state.password }
+            name='password'
+            onChange={ e => this.handleUserInput(e) }
+          />
+          <button
+            className='login-button'
+            onClick={ () => this.handleUserSubmit() } >
+            Login
+          </button>
+        </article>
+      </div>
     );
   }
 }
