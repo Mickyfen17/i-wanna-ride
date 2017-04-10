75d8302a020ff48ee2517402df789bc7a6c7de59
Michael Fenwick <mickyfen17@aol.com>
2017-04-10 11:13:05 -0600

    add basic form and alter state upon change

--- app/components/CreateUser.js
+++ app/components/CreateUser.js
@@ -7,7 +7,21 @@ export default class CreateUser extends Component  {
   constructor() {
     super();
     this.state = {
+      firstname: '',
+      lastname: '',
+      location: '',
+      experience: '',
+      email: '',
+      username: '',
+      password: '',
     };
+  }
+
+  handleUserInput(e) {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
@@ -15,6 +29,62 @@ export default class CreateUser extends Component  {
       <div>
         <Header />
         <h1>Create User</h1>
+        <input
+          className='create-user-input'
+          placeholder='First Name'
+          type='text'
+          value={ this.state.firstname }
+          name='firstname'
+          onChange={ e => this.handleUserInput(e) }
+        />
+        <input
+          className='create-user-input'
+          placeholder='Last Name'
+          type='text'
+          value={ this.state.lastname }
+          name='lastname'
+          onChange={ e => this.handleUserInput(e) }
+        />
+        <input
+          className='create-user-input'
+          placeholder='Location'
+          type='text'
+          value={ this.state.location }
+          name='location'
+          onChange={ e => this.handleUserInput(e) }
+        />
+        <input
+          className='create-user-input'
+          placeholder='Experience'
+          type='text'
+          value={ this.state.experience }
+          name='experience'
+          onChange={ e => this.handleUserInput(e) }
+        />
+        <input
+          className='create-user-input'
+          placeholder='Email'
+          type='text'
+          value={ this.state.email }
+          name='email'
+          onChange={ e => this.handleUserInput(e) }
+        />
+        <input
+          className='create-user-input'
+          placeholder='Username'
+          type='text'
+          value={ this.state.username }
+          name='username'
+          onChange={ e => this.handleUserInput(e) }
+        />
+        <input
+          className='create-user-input'
+          placeholder='Password'
+          type='password'
+          value={ this.state.password }
+          name='password'
+          onChange={ e => this.handleUserInput(e) }
+        />
       </div>
     );
   }
