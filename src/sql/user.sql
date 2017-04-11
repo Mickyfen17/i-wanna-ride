ab31b79e90c97ce2c2e284d7d58c50566c604ddc
Michael Fenwick <mickyfen17@aol.com>
2017-04-11 17:33:59 -0600

    alter user table & add UNIQUE to username

--- src/sql/user.sql
+++ src/sql/user.sql
@@ -5,28 +5,14 @@ CREATE DATABASE users;
 
 CREATE TABLE users (
   id SERIAL NOT NULL PRIMARY KEY,
-  first_name VARCHAR (123) NOT NULL,
-  last_name VARCHAR (123) NOT NULL,
+  firstname VARCHAR (123) NOT NULL,
+  lastname VARCHAR (123) NOT NULL,
   location VARCHAR (123) NOT NULL,
   experience VARCHAR (123) NOT NULL,
-  username VARCHAR (123) NOT NULL,
-  email VARCHAR (123) NOT NULL,
+  username VARCHAR (123) NOT NULL UNIQUE,
+  email VARCHAR (123) NOT NULL UNIQUE,
   password VARCHAR (123) NOT NULL
 );
 
--- CREATE TABLE favorites (
---   id SERIAL NOT NULL PRIMARY KEY,
---   movie_id INTEGER NOT NULL,
---   user_id INTEGER NOT NULL,
---   title VARCHAR (123) NOT NULL,
---   poster_path VARCHAR (250) NOT NULL,
---   release_date VARCHAR (123) NOT NULL,
---   vote_average VARCHAR (123) NOT NULL,
---   overview VARCHAR NOT NULL
--- );
-
-CREATE UNIQUE INDEX email ON users (email);
-
-
-INSERT INTO users (first_name, last_name, location, experience, username, password, email)
+INSERT INTO users (firstname, lastname, location, experience, username, password, email)
   VALUES ('Mike', 'Fenwick', 'Denver', 'Intermediate', 'mikefen', 'password', 'abc@abc.com');
