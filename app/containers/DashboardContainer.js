0470897d37ccd6deba33ec29353262ac1aba97a4
Michael Fenwick <mickyfen17@aol.com>
2017-04-11 22:45:16 -0600

    map userSignOut to props for Dashboard

--- app/containers/DashboardContainer.js
+++ app/containers/DashboardContainer.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 
 import Dashboard from '../components/Dashboard';
+import { userSignOut } from '../actions/actions';
+
 
 const mapStateToProps = (state) => {
   return state;
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return dispatch;
+  return {
+    userSignOut: () => dispatch(userSignOut()),
+  };
 };
 
-export default connect(mapStateToProps, null)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
