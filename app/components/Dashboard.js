d862830829807ba9087e19059e0f9f6cd1a8cfe7
Michael Fenwick <mickyfen17@aol.com>
2017-04-11 22:23:05 -0600

    add condition to display first name on sign in

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -3,14 +3,15 @@ import React from 'react';
 import Header from './Header';
 
 const Dashboard = ({ user: { signedIn, firstname } }) => {
+  const userSignedIn = signedIn ? <h3>Welcome back, { firstname }</h3> : <h3>Please Login</h3>;
   return (
-    <div>
+    <div className='dashboard'>
       <Header
         className={ 'signed-in-header' }
         signedIn={ signedIn }
-        firstname={ firstname }
       />
       <h1>DASHBOARD</h1>
+      { userSignedIn }
     </div>
   );
 };
