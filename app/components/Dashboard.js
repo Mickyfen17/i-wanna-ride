03cca2b8ca3e7c9affca6f820059caa17b7218ef
Michael Fenwick <mickyfen17@aol.com>
2017-04-11 22:43:42 -0600

    pass userSignOut & history to Header

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Header from './Header';
 
-const Dashboard = ({ user: { signedIn, firstname } }) => {
+const Dashboard = ({ user: { signedIn, firstname }, userSignOut, history }) => {
   const userSignedIn = signedIn ? <h3>Welcome back, { firstname }</h3> : <h3>Please Login</h3>;
   return (
     <div className='dashboard'>
       <Header
         className={ 'signed-in-header' }
         signedIn={ signedIn }
+        handleSignOut={ userSignOut }
+        history={ history }
       />
       <h1>DASHBOARD</h1>
       { userSignedIn }
