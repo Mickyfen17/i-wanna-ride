868d242a1686e6dc2aadffec6e491721ae6e9765
Michael Fenwick <mickyfen17@aol.com>
2017-04-13 16:59:09 -0600

    add modal to show user details on ride match

--- app/components/RideInfo.js
+++ app/components/RideInfo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 
 export default class RideInfo extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       matchedRides: [],
-    }
+      showMatchDetails: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   componentWillMount() {
@@ -25,18 +28,55 @@ export default class RideInfo extends Component {
     );
   }
 
+  toggleModal() {
+    this.setState({
+      showMatchDetails: !this.state.showMatchDetails,
+    });
+  }
+
+  matchedRideDetails() {
+    const { showMatchDetails, matchedRides } = this.state;
+    return (
+    <Modal
+      isOpen={ showMatchDetails }
+      contentLabel='matched-rider-info'
+    >
+      <article>
+        <h1>Rider Infomation</h1>
+        {
+          matchedRides.map((ride, i) =>
+            <div key={ i }>
+              <h2>{ ride.firstname }</h2>
+              <h2>{ ride.email }</h2>
+            </div>,
+          )
+        }
+        <button
+          onClick={ this.toggleModal } >
+          X
+        </button>
+      </article>
+    </Modal>
+    );
+  }
+
   render() {
     const { matchedRides } = this.state;
     const { date, time, experience, location } = this.props;
+    const userModal = this.matchedRideDetails();
     return (
       <tr className='ride-row'>
+        { userModal }
         <td className='ride-col'>
           { `${date} - ${time} - ${experience} - ${location}` }
         </td>
         <td className='ride-col'>
           {
             matchedRides.length > 0 ?
-            <p>Ride Matched</p> :
+            <button
+              onClick={ this.toggleModal } >
+              Ride Matched
+            </button> :
             <p>No Matches</p>
           }
         </td>
