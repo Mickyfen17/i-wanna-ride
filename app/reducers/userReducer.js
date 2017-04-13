e0fb81bbf3d5095a03dcf4a44e3c6d51b1c31140
Michael Fenwick <mickyfen17@aol.com>
2017-04-13 08:34:47 -0600

    add USER_SIGNED_OUT into rootReducer to reset to default state

--- app/reducers/userReducer.js
+++ app/reducers/userReducer.js
@@ -14,8 +14,6 @@ const user = (state = intialState, action) => {
   switch (action.type) {
     case 'USER_SIGNED_IN' :
       return Object.assign({}, state, action.user, { signedIn: true });
-    case 'USER_SIGNED_OUT' :
-      return intialState;
 
     default:
       return state;
