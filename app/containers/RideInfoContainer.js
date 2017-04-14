5f606c928a61cec472b452dda73b15363ff45d5e
Michael Fenwick <mickyfen17@aol.com>
2017-04-13 18:58:08 -0600

    pass fetchUserRides action to RideInfo

--- app/containers/RideInfoContainer.js
+++ app/containers/RideInfoContainer.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 
 import RideInfo from '../components/RideInfo';
-import { fetchMatchedRides } from '../actions/actions';
+import { fetchMatchedRides, fetchUserRides } from '../actions/actions';
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchMatchedRides: (userID, date, time, experience, location) =>
       dispatch(fetchMatchedRides(userID, date, time, experience, location)),
+    fetchUserRides: rides => dispatch(fetchUserRides(rides)),
   };
 };
 
