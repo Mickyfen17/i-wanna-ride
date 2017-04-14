8b3d1af0a39cbee619b6ae3bdfedd263149061ac
Michael Fenwick <mickyfen17@aol.com>
2017-04-14 15:58:35 -0600

    pass deleteRideCall func from state to RideInfo

--- app/containers/RideInfoContainer.js
+++ app/containers/RideInfoContainer.js
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 
 import RideInfo from '../components/RideInfo';
-import { fetchMatchedRides, fetchUserRides } from '../actions/actions';
+import { fetchMatchedRides, fetchUserRides, deleteRide } from '../actions/actions';
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchMatchedRides: (userID, date, time, experience, location) =>
       dispatch(fetchMatchedRides(userID, date, time, experience, location)),
     fetchUserRides: rides => dispatch(fetchUserRides(rides)),
+    deleteRideCall: (user_id, ride_id) => dispatch(deleteRide(user_id, ride_id)),
   };
 };
 
