9e23fe3902eb588169d296e04b8af6ee8c93cd87
Michael Fenwick <mickyfen17@aol.com>
2017-04-15 12:22:58 -0600

    app MapMarker component to add pins to google map

---
+++ app/components/MapMarker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MapMarker extends Component {
+
+  render() {
+    return (
+      <div className='map-marker'>
+      </div>
+    );
+  }
+}
