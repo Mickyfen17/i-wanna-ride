f38527129b22c7abed400284a7f8378fa761cf89
Michael Fenwick <mickyfen17@aol.com>
2017-04-15 14:19:52 -0600

    remove React Component from App

--- app/components/App.js
+++ app/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from './Home';
@@ -7,17 +7,16 @@ import CreateUserContainer from '../containers/CreateUserContainer';
 import DashboardContainer from '../containers/DashboardContainer';
 import CreateRideContainer from '../containers/CreateRideContainer';
 
-export default class App extends Component {
+const App = () => {
+  return (
+    <div>
+      <Route exact path='/' component={ Home } />
+      <Route path='/login' component={ LoginContainer } />
+      <Route path='/create-user' component={ CreateUserContainer } />
+      <Route path='/dashboard' component={ DashboardContainer } />
+      <Route path='/create-ride' component={ CreateRideContainer } />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <Route exact path='/' component={ Home } />
-        <Route path='/login' component={ LoginContainer } />
-        <Route path='/create-user' component={ CreateUserContainer } />
-        <Route path='/dashboard' component={ DashboardContainer } />
-        <Route path='/create-ride' component={ CreateRideContainer } />
-      </div>
-    );
-  }
-}
+export default App;
