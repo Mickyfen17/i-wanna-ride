a52ac783881b3fb63787c0386ae5802da28e3ee4
Michael Fenwick <mickyfen17@aol.com>
2017-04-15 13:43:15 -0600

    add href to google maps for directions

--- app/components/MapMarker.js
+++ app/components/MapMarker.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 export default class MapMarker extends Component {
 
   render() {
+    const { location, lat, lng } = this.props;
     return (
-      <div className='map-marker'>
-      </div>
+      <a
+        href={`https://www.google.com/maps/place/${location}/@${lat},${lng},15z/`}
+        target='_blank'
+        >
+        <div
+          className='map-marker'
+        >
+        </div>
+      </a>
     );
   }
 }
