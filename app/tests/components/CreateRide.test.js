8408fa70e09d9047d97d674806b2be267f436182
Michael Fenwick <mickyfen17@aol.com>
2017-04-18 22:04:13 -0600

    add first test to CreateRide

---
+++ app/tests/components/CreateRide.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import fetchMock from 'fetch-mock';
+import { BrowserRouter } from 'react-router-dom'
+import CreateRide from '../../components/CreateRide';
+import Input from '../../components/Input';
+
+
+describe('testing CreateRide', () => {
+  const defaultUser = {
+    signedIn: false,
+  };
+  const singedInMockUser = {
+    username: 'mikefen',
+    password: 'password',
+    signedIn: true,
+  };
+
+  afterEach(() => {
+    expect(fetchMock.calls().unmatched).toEqual([]);
+    fetchMock.restore();
+  });
+
+  it('should have a default state', () => {
+    const wrapper = shallow( <CreateRide user={ defaultUser } /> )
+
+    expect(wrapper.state())
+    .toEqual({ 'experience': 'Beginner', 'latitude': '', 'location': '', 'longitude': '', 'ridedate': '', 'ridetime': 'Morning'})
+  })
+
+  it('be able to add text to inputs and update state', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <CreateRide user={ defaultUser } />
+      </BrowserRouter>,
+    );
+
+    console.log(wrapper.debug());
+
+    const createRideWrapper = wrapper.find(CreateRide);
+
+    const locationInput = wrapper.find('.geosuggest__input');
+    const experienceInput = wrapper.find('select[name="experience"]');
+    const dateInput = wrapper.find('input[name="ridedate"]');
+    const timeInput = wrapper.find('select[name="ridetime"]');
+
+    console.log(locationInput);
+    // console.log(experienceInput.debug());
+    // console.log(dateInput.debug());
+    // console.log(timeInput.debug());
+
+    locationInput.simulate('change', {
+      target: {
+        name: 'location',
+        value: 'Denver',
+      },
+    });
+    dateInput.simulate('change', {
+      target: {
+        name: 'ridedate',
+        value: '01/01/2018',
+      },
+    });
+
+    console.log(createRideWrapper.node.state);
+
+    expect(createRideWrapper.node.state)
+    .toEqual({ 'location': 'Denver', 'experience': 'Beginner', 'ridedate': '01/01/2018', 'ridetime': 'Morning'})
+  });
+
+});
