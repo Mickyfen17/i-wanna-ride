ad3f84e41ed5a61185f3c2d042222134d11fc73e
Michael Fenwick <mickyfen17@aol.com>
2017-04-18 23:36:54 -0600

    add conditional rendering to routes for user signedIn

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,5 @@
 import React  from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import Home from './Home';
 import LoginContainer from '../containers/LoginContainer';
@@ -7,14 +7,26 @@ import CreateUserContainer from '../containers/CreateUserContainer';
 import DashboardContainer from '../containers/DashboardContainer';
 import CreateRideContainer from '../containers/CreateRideContainer';
 
-const App = () => {
+const App = ({ user: { signedIn } }) => {
   return (
     <div>
       <Route exact path='/' component={ Home } />
       <Route path='/login' component={ LoginContainer } />
       <Route path='/create-user' component={ CreateUserContainer } />
-      <Route path='/dashboard' component={ DashboardContainer } />
-      <Route path='/create-ride' component={ CreateRideContainer } />
+      <Route path="/dashboard" render={() => (
+        !signedIn ? (
+          <Redirect to="/login"/>
+        ) : (
+          <DashboardContainer />
+        )
+      )}/>
+      <Route path="/create-ride" render={() => (
+        !signedIn ? (
+          <Redirect to="/login"/>
+        ) : (
+          <CreateRideContainer />
+        )
+      )}/>
     </div>
   );
 };
