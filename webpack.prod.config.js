06fdba4496aca49eb45c2bfd238c44e53bd765f1
Michael Fenwick <mickyfen17@aol.com>
2017-04-26 17:12:05 -0700

    alter webpack configs to allow hot loading and prod build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'app'),
     filename: 'bundle.js',
-    // publicPath: '/assets/',
+    publicPath: '/assets/',
   },
   module: {
     loaders: [
